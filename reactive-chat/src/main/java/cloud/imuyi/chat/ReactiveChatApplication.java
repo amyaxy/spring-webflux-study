b17842cec89c42c75223011b93db6b33b8144817
keep simple <3132670669@qq.com>
2026-07-17 11:20:44 +0800

    feat: 新增 reactive-chat 模块骨架（R2DBC + H2 + VT + Actuator）

---
+++ reactive-chat/src/main/java/cloud/imuyi/chat/ReactiveChatApplication.java
@@ -0,0 +1,11 @@
+package cloud.imuyi.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveChatApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveChatApplication.class, args);
+    }
+}
